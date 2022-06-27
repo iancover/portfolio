e9b7f729689edd747927247168b7731ccbb8246e
Ian Cover <iancover@gmail.com>
2022-06-27 12:26:54 -0500

    feat: Add content for Contact page.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,28 @@
-function Resume() {
+import { Link } from 'react-router-dom';
+import { SiLinkedin } from 'react-icons/si';
+
+function Contact() {
   return (
-    <div>
-      <h1>Resume</h1>
+    <div className='text-center'>
+      <header className='bg-secondary rounded py-20 m-45'>
+        <h1 className='text-orange'>Contact Me</h1>
+      </header>
+      <main className='bg-secondary rounded py-20 px-30 m-45'>
+        <h3 className='text-dark'>Inbox</h3>
+        <Link to='https://www.linkedin.com/messaging/' className='text-blue'>
+          <SiLinkedin className='mr-05' /> linkedin.com/in/iancover
+        </Link>
+        <h3 className='text-dark'>Email</h3>
+        <Link to='mailto:iancover@gmail.com' className='text-blue'>
+          iancover@gmail.com
+        </Link>
+        <h3 className='text-dark'>Phone</h3>
+        <Link to='tel:5122003707' className='text-blue'>
+          512.200.3707
+        </Link>
+      </main>
     </div>
   );
 }
 
-export default Resume;
+export default Contact;
