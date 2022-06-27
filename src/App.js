8c9a1d97788378465b3ababaa1bcdf8df80fd7d4
Ian Cover <iancover@gmail.com>
2022-06-27 12:03:00 -0500

    feat: Add routes to Home, About, Projects and Contact pages.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Pages
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import About from './pages/About';
+import Contact from './pages/Contact';
+// Components
 import Navbar from './components/Navbar';
 
 function App() {
@@ -6,6 +12,12 @@ function App() {
     <>
       <Router>
         <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
       </Router>
     </>
   );
