3bcdcea841cbc0212a14664056419f0698fb813d
Ian Cover <iancover@gmail.com>
2022-06-27 13:40:37 -0500

    feat: Add about page content.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,36 @@
 function AboutMe() {
   return (
-    <div>
-      <h1>AboutMe</h1>
+    <div className='text-center'>
+      <header className='bg-secondary rounded py-20 m-45'>
+        <h1 className='text-orange'>About Me</h1>
+        <p className='text-dark'>
+          Web developer specializing in JavaScript with a focus on the MERN
+          stack.
+          <br />
+        </p>
+      </header>
+      <main className='bg-secondary rounded py-20 px-30 m-45'>
+        <h3 className='text-dark'>Bio</h3>
+        <p className='text-dark'>
+          Moved to Austin, TX in 2012 to further my career in the tech industry
+          and worked for multiple software start-ups in sales roles covering
+          diverse verticals from domestic Mid-markets to international
+          Enterprise levels; with increased exposure to the technical side of
+          the SaaS industry at every turn.
+          <br />
+          After a couple of years of entertaining the idea of a career
+          transition into programming, in 2017 I enrolled in a full stack web
+          development bootcamp. Near completion of the program later that year,
+          feeling solely equipped with the fundamentals, rather than rushing
+          into the field partially prepared, I decided to delay my plans and
+          take that next step only when the timing felt right. I have since been
+          focused on learning all aspects of the full stack developer path,
+          working on multiple projects to further my experience, while faced
+          with multiple challenges, including a global pandemic.
+          <br />
+          Thanks for reading!
+        </p>
+      </main>
     </div>
   );
 }
