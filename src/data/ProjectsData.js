9758220f196e04da3c0664d2e2cefbd84f14dcfe
Ian Cover <iancover@gmail.com>
2022-06-28 18:03:56 -0500

    Send project info from stored data file

---
+++ src/data/ProjectsData.js
@@ -0,0 +1,45 @@
+export const projectsList = () => {
+  return [
+    {
+      title: 'PlayerCare',
+      technologies:
+        'React, Redux Toolkit, Node, Express, MongoDB, Mongoose, Heroku',
+      description:
+        'A client-server REST application help-desk, designed for a tech support team at a fictitious video-game developing company.',
+      link: 'https://playercare.herokuapp.com/',
+      id: 1,
+    },
+    {
+      title: 'DevCamper',
+      technologies: 'Node, Express, MongoDB, Mongoose, Postman',
+      description:
+        'Node/Express REST API built as a backend for a web development bootcamp directory using geocoding info. Documentation built with Postman.',
+      link: 'https://documenter.getpostman.com/view/2204758/TVKEWGxF',
+      id: 2,
+    },
+    {
+      title: 'Home-In',
+      technologies: 'React, Firebase, Firestore, Vercel',
+      description:
+        'A mobile first React application, designed to build a real-estate marketplace for home buyers, sellers and renters.',
+      link: 'https://home-in.vercel.app/',
+      id: 3,
+    },
+    {
+      title: 'Userch',
+      technologies: 'React, React Context, GitHub API',
+      description:
+        'React UI application to search for GitHub user profiles and repositories using the GitHub API and React Context to manage state.',
+      link: 'https://userch.vercel.app/',
+      id: 4,
+    },
+    {
+      title: 'critIQ',
+      technologies: 'React, Framer Motion, Netlify',
+      description:
+        'React UI application for submitting reviews and ratings, using Framer Motion component library and deployed to Netflify.',
+      link: 'https://critiq.netlify.app/',
+      id: 5,
+    },
+  ];
+};
